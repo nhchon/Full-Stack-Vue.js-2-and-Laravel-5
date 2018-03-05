411c92b092b76fa18aa1e83a88edb7d6bd68118b
Chon Nguyen <cnguyen@metabiota.com>
2018-03-05 17:36:10 +0700

    Learning chapter 2

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		title: sample.title,
+		address: sample.address,
+		about: sample.about,
+		headerImageStyle: {
+			'background-image': 'url(sample/header.jpg)'
+		},
+		amenities: sample.amenities,
+		prices: sample.prices
+	}
+});
