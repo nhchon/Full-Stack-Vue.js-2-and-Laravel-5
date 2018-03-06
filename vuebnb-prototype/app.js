791a8c5a9ec7e14fd3adcae75f9bceebe90ee4ef
Chon Nguyen <cnguyen@metabiota.com>
2018-03-06 18:24:16 +0700

    continued learning chapter 02

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -9,6 +9,17 @@ var app = new Vue({
 		},
 		amenities: sample.amenities,
 		prices: sample.prices,
-		contracted: true
+		contracted: true,
+		modalOpen: false
+	},
+	watch: {
+		modalOpen: function() {
+			var className = 'modal-open';
+			if (this.modalOpen) {
+				document.body.classList.add(className);
+			} else {
+				document.body.classList.remove(className);
+			}
+		}
 	}
 });
